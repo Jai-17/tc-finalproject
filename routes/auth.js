8fb9cc843486ba810ff9292c6301cc17e36c2105
Jai Madhukar <madhukarjai@gmail.com>
2022-12-02 08:11:19 +0530

    google auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,36 @@
 const router = require('express').Router();
+const axios = require('axios');
+const urlParse = require('url-parse');
+const qs = require('query-string');
+require('dotenv').config()
+
+const redirectUri = process.env.REDIRECT_URI
+
+function getGoogleAuthURL(){
+
+    const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth'
+    const options = {
+        redirectUri : redirectUri,
+        client_id : process.env.GOOGLE_CLIENT_ID,
+        access_type : 'offline',
+        response_type : 'code',
+        prompt : 'consent',
+        scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ].join(" "),
+    }
+
+    return `${rootUrl}?${qs.stringify(options)}`
+
+}
+
+router.get('/', (req,res) => {
+    res.send('Auth Route')
+})
 
 router.get('/login', (req, res) => {
-    req.send('Login Page')
+    res.redirect(getGoogleAuthURL())
 })
 
 router.get('/logout', (req, res) => {
